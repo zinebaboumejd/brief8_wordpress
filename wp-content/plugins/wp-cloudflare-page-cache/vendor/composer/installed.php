ea55bf47b8a9bc5d5cbc6e9c7cbd2eaaef4b50bd
zinebaboumejd <zinebaboumejd@gmail.com>
2022-04-29 16:08:38 +0000

    v1

---
+++ wp-content/plugins/wp-cloudflare-page-cache/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'v4.5.8',
+        'version' => '4.5.8.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd59e4f2450e87f253a4526290a00f4e516dac24a',
+        'name' => 'codeinwp/wp-cloudflare-super-page-cache',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/wp-cloudflare-super-page-cache' => array(
+            'pretty_version' => 'v4.5.8',
+            'version' => '4.5.8.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd59e4f2450e87f253a4526290a00f4e516dac24a',
+            'dev_requirement' => false,
+        ),
+        'deliciousbrains/wp-background-processing' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../deliciousbrains/wp-background-processing',
+            'aliases' => array(),
+            'reference' => '2cbee1abd1b49e1133cd8f611df4d4fc5a8b9800',
+            'dev_requirement' => false,
+        ),
+    ),
+);
