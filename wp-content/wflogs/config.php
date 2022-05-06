5ff838f2a405e9eba447f19035e02bea6a1ba683
zinebaboumejd <zinebaboumejd@gmail.com>
2022-05-06 11:59:35 +0000

    v2

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1651748335;s:7:"authKey";s:64:"?S4C31D,{e ple=Yad3_1SgW<?fZ>mSfqO`4{G,pfNT({rf1~KkF_]uJ~pnuOEZv";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3282;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"?S4C31D,{e ple=Yad3_1SgW<?fZ>mSfqO`4{G,pfNT({rf1~KkF_]uJ~pnuOEZv";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3282;}
